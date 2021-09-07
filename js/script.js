ae048c64c0ab7085cee50c839b47cc43e881ce15
omjee17 <singhomjee123@gmail.com>
2021-09-08 00:30:46 +0530

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var typed=new Typed('.animate',{
+    strings:[
+        "Web Developer.",
+        "Competitive Programmer.",
+        "Computer Science Gradute.",
+        "Proud Indian.",
+        ],
+        typeSpeed:50,
+        backSpeed:50,
+        loop:true,
+})
